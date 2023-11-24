c11eca3ad76e836a942a5d56f5de24e107eabc5d
qianxuege <qianxuege15@gmail.com>
2023-11-24 18:44:21 -0500

    in the process of created class for board. now creating class for cell

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from handDetection import runCamera
 
 class Board:
     def __init__(self, width, height, rows, cols):
-        self.width = width #350
-        self.height = height #350
+        self.width = width #500
+        self.height = height #500
         self.rows = rows #7
         self.cols = cols #7
         self.cellBorderWidth = 1
-        self.boardLeft = 50
-        self.boardTop = 150
+        self.boardLeft = 100
+        self.boardTop = 100
     
     def drawBoard(self):
         # width, height = self.width, self.height
         # rows, cols = self.rows, self.cols
         for row in range(self.rows):
             for col in range(self.cols):
+                if 0 < row < (self.rows-1)  and 0 < col < (self.cols-1):
+                    continue
                 self.drawCell(row, col)
         self.drawBoardBorder()
     
@@ -23,6 +25,10 @@ class Board:
         drawRect(self.boardLeft, self.boardTop, self.width, self.height, 
                  fill=None, border = 'black', 
                  borderWidth = 2*self.cellBorderWidth)
+        cellSize = self.getCellSize()
+        drawRect(self.boardLeft + cellSize, self.boardTop + cellSize, self.width - (2*cellSize), self.height - (2*cellSize),
+                 fill=None, border = 'black',
+                 borderWidth = self.cellBorderWidth)
     
     def drawCell(self, row, col):
         cellLeft, cellTop = self.getCellLeftTop(row, col)
@@ -39,21 +45,79 @@ class Board:
     
     def getCellSize(self):
         cellSize = self.width/self.cols
-        # print(cellSize)
         return cellSize
         
+class Cell:
+    def __init__(self, secretType, cellLeft, cellTop, cellSize):
+        self.secretType = secretType
+        self.cellLeft = cellLeft
+        self.cellTop = cellTop
+        self.cellSize = cellSize
+    def __repr__(self):
+        return f'{self.secretType}'       
+        
+    def __eq__(self, other):
+        return (isinstance(other, Cell) and (self.secretType==other.secretType)
+                and (self.cellLeft == other.cellLeft) and (self.cellTop == other.cellTop))
+        
+    def __hash__(self):
+        return hash(str(self))
+    
+    def drawCellType(self):
+        drawLabel(self.secretType, self.cellLeft + .5*self.cellSize, self.cellTop + .5*self.cellSize)
+    ## print the labels (secretType)on the cell
+
+class Secret(Cell):
+    def __init__(self):
+        self.secrets = []
+
+class Rooms:
+    def __init__(self, name, character, characterSecret, isRoom): # isRoom is a boolean value
+        self.name = name
+        self.accessible = True
+        self.character = character
+        # the secrets
+        self.characterSecret = characterSecret
+        self.isRoom = isRoom
+        # ownserships
+        self.characterSecretOwner = None
+        self.isRoomOwner = None
+        
+    def __repr__(self):
+        return f'{self.name}({self.character}, {self.characterSecretOwner}, {self.isRoomOwner})'
+    def __hash__(self):
+        return hash(str(self))
+    def __eq__(self, other):
+        return isinstance(other, Rooms) and (other.name == self.name)
+    
+    def buySecret(self, player):
+        #
+        pass
+    
+    def checkSecret(self):
+        if self.characterSecretOwner == None:
+            return f'You can buy a character secret'
+        elif self.isRoomOwner == None:
+            return f'You can buy a room secret'
+        else:
+            return None
+    
+
+
+        
+    
 
 def onAppStart(app):
-    app.height = 600
-    app.width = 600
+    app.height = 700
+    app.width = 800
     app.paused = True
     app.stepsPerSecond = 5
     
 
 def redrawAll(app):
-    drawLabel('112 Murder Mystery', 200, 200)
+    # drawLabel('112 Murder Mystery', 200, 200)
     # drawLabel(app.paused, 200, 250)
-    gameBoard = Board(400, 400, 7, 7)
+    gameBoard = Board(500, 500, 7, 7)
     gameBoard.drawBoard()
 
 def onKeyPress(app, key):
